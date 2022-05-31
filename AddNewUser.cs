46ada94de9deb3f107fc5c969e7b0d8c68a10c0c
lauranton <95312859+lauranton@users.noreply.github.com>
2022-05-31 22:49:41 +0300

    Add files via upload

---
+++ AddNewUser.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DermaFram
+{
+    public partial class AddNewUser : Form
+    {
+        User newuser = new User();
+
+        public AddNewUser()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AddNewUser_Load(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        private void HireDate_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void newUserUsername_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void newUserpassword_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NewUserCancel_Click(object sender, EventArgs e)
+        {
+            Clear();
+
+        }
+
+        void Clear()
+        {
+            txtnewuseName.Text = txtHireDate.Text = newUserUsername.Text = newUserpassword.Text = "";
+            newUserAdd.Text = "Save";
+            NewUserDelete.Enabled = false;
+            newuser.userID = 0;
+
+        }
+
+        private void newUserAdd_Click(object sender, EventArgs e)
+        {
+            newuser.Name = txtnewuseName.Text.Trim();
+            newuser.username = txtNewUsername.Text.Trim();
+            newuser.password = txtnewuserPassword.Text.Trim();
+            newuser.hireDate = txtHireDate.Text.Trim();
+            using (DBEntities db = new DBEntities())
+            {
+                db.Users.Add(newuser);
+                db.SaveChanges();
+            }
+            Clear();
+            MessageBox.Show("Submittesd Successfully");
+
+        }
+    }
+}
---
+++ Administrator.Designer.cs
@@ -0,0 +1,277 @@
+﻿
+namespace DermaFram
+{
+    partial class Administrator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Administrator));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnLogOut = new System.Windows.Forms.Panel();
+            this.LogOutBtn = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Button3 = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Button2 = new Guna.UI2.WinForms.Guna2Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.guna2Elipse1 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
+            this.closeButton = new Guna.UI2.WinForms.Guna2Button();
+            this.pharm = new Guna.UI2.WinForms.Guna2Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel1.Controls.Add(this.pharm);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.btnLogOut);
+            this.panel1.Controls.Add(this.LogOutBtn);
+            this.panel1.Controls.Add(this.guna2Button3);
+            this.panel1.Controls.Add(this.guna2Button2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(416, 752);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(507, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(991, 999);
+            this.panel2.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(175, 871);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(174, 46);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "username";
+            // 
+            // btnLogOut
+            // 
+            this.btnLogOut.Location = new System.Drawing.Point(503, 2);
+            this.btnLogOut.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnLogOut.Name = "btnLogOut";
+            this.btnLogOut.Size = new System.Drawing.Size(1441, 999);
+            this.btnLogOut.TabIndex = 1;
+            // 
+            // LogOutBtn
+            // 
+            this.LogOutBtn.ButtonMode = Guna.UI2.WinForms.Enums.ButtonMode.RadioButton;
+            this.LogOutBtn.CheckedState.FillColor = System.Drawing.Color.White;
+            this.LogOutBtn.CheckedState.ForeColor = System.Drawing.Color.Black;
+            this.LogOutBtn.CheckedState.Parent = this.LogOutBtn;
+            this.LogOutBtn.CustomImages.Parent = this.LogOutBtn;
+            this.LogOutBtn.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.LogOutBtn.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.LogOutBtn.ForeColor = System.Drawing.Color.White;
+            this.LogOutBtn.HoverState.Parent = this.LogOutBtn;
+            this.LogOutBtn.Image = ((System.Drawing.Image)(resources.GetObject("LogOutBtn.Image")));
+            this.LogOutBtn.ImageSize = new System.Drawing.Size(40, 40);
+            this.LogOutBtn.Location = new System.Drawing.Point(35, 785);
+            this.LogOutBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.LogOutBtn.Name = "LogOutBtn";
+            this.LogOutBtn.ShadowDecoration.Parent = this.LogOutBtn;
+            this.LogOutBtn.Size = new System.Drawing.Size(435, 73);
+            this.LogOutBtn.TabIndex = 8;
+            this.LogOutBtn.Text = "Log Out";
+            this.LogOutBtn.Click += new System.EventHandler(this.guna2Button5_Click);
+            // 
+            // guna2Button3
+            // 
+            this.guna2Button3.ButtonMode = Guna.UI2.WinForms.Enums.ButtonMode.RadioButton;
+            this.guna2Button3.CheckedState.FillColor = System.Drawing.Color.White;
+            this.guna2Button3.CheckedState.ForeColor = System.Drawing.Color.Black;
+            this.guna2Button3.CheckedState.Parent = this.guna2Button3;
+            this.guna2Button3.CustomImages.Parent = this.guna2Button3;
+            this.guna2Button3.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.guna2Button3.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.guna2Button3.ForeColor = System.Drawing.Color.White;
+            this.guna2Button3.HoverState.Parent = this.guna2Button3;
+            this.guna2Button3.Image = ((System.Drawing.Image)(resources.GetObject("guna2Button3.Image")));
+            this.guna2Button3.ImageSize = new System.Drawing.Size(40, 40);
+            this.guna2Button3.Location = new System.Drawing.Point(12, 617);
+            this.guna2Button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.guna2Button3.Name = "guna2Button3";
+            this.guna2Button3.ShadowDecoration.Parent = this.guna2Button3;
+            this.guna2Button3.Size = new System.Drawing.Size(372, 91);
+            this.guna2Button3.TabIndex = 6;
+            this.guna2Button3.Text = "Log out";
+            // 
+            // guna2Button2
+            // 
+            this.guna2Button2.ButtonMode = Guna.UI2.WinForms.Enums.ButtonMode.RadioButton;
+            this.guna2Button2.CheckedState.FillColor = System.Drawing.Color.White;
+            this.guna2Button2.CheckedState.ForeColor = System.Drawing.Color.Black;
+            this.guna2Button2.CheckedState.Parent = this.guna2Button2;
+            this.guna2Button2.CustomImages.Parent = this.guna2Button2;
+            this.guna2Button2.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.guna2Button2.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.guna2Button2.ForeColor = System.Drawing.Color.White;
+            this.guna2Button2.HoverState.Parent = this.guna2Button2;
+            this.guna2Button2.Image = ((System.Drawing.Image)(resources.GetObject("guna2Button2.Image")));
+            this.guna2Button2.ImageSize = new System.Drawing.Size(40, 40);
+            this.guna2Button2.Location = new System.Drawing.Point(24, 454);
+            this.guna2Button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.guna2Button2.Name = "guna2Button2";
+            this.guna2Button2.ShadowDecoration.Parent = this.guna2Button2;
+            this.guna2Button2.Size = new System.Drawing.Size(375, 73);
+            this.guna2Button2.TabIndex = 5;
+            this.guna2Button2.Text = "Add New User";
+            this.guna2Button2.Click += new System.EventHandler(this.guna2Button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(109, 372);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(218, 59);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Dashbord";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(39, 43);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(324, 317);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // guna2Elipse1
+            // 
+            this.guna2Elipse1.TargetControl = this.panel2;
+            // 
+            // closeButton
+            // 
+            this.closeButton.BackColor = System.Drawing.Color.Transparent;
+            this.closeButton.CheckedState.Parent = this.closeButton;
+            this.closeButton.CustomImages.Parent = this.closeButton;
+            this.closeButton.FillColor = System.Drawing.Color.White;
+            this.closeButton.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.closeButton.ForeColor = System.Drawing.Color.White;
+            this.closeButton.HoverState.Parent = this.closeButton;
+            this.closeButton.Image = ((System.Drawing.Image)(resources.GetObject("closeButton.Image")));
+            this.closeButton.Location = new System.Drawing.Point(1445, -14);
+            this.closeButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.ShadowDecoration.Parent = this.closeButton;
+            this.closeButton.Size = new System.Drawing.Size(68, 70);
+            this.closeButton.TabIndex = 12;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // pharm
+            // 
+            this.pharm.ButtonMode = Guna.UI2.WinForms.Enums.ButtonMode.RadioButton;
+            this.pharm.CheckedState.FillColor = System.Drawing.Color.White;
+            this.pharm.CheckedState.ForeColor = System.Drawing.Color.Black;
+            this.pharm.CheckedState.Parent = this.pharm;
+            this.pharm.CustomImages.Parent = this.pharm;
+            this.pharm.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.pharm.Font = new System.Drawing.Font("Century Gothic", 13.8F);
+            this.pharm.ForeColor = System.Drawing.Color.White;
+            this.pharm.HoverState.Parent = this.pharm;
+            this.pharm.Image = ((System.Drawing.Image)(resources.GetObject("pharm.Image")));
+            this.pharm.ImageSize = new System.Drawing.Size(40, 40);
+            this.pharm.Location = new System.Drawing.Point(12, 540);
+            this.pharm.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pharm.Name = "pharm";
+            this.pharm.ShadowDecoration.Parent = this.pharm;
+            this.pharm.Size = new System.Drawing.Size(375, 73);
+            this.pharm.TabIndex = 13;
+            this.pharm.Text = "Pharmacy";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(454, 73);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(800, 600);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 13;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Administrator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1300, 750);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Administrator";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Administrator";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel btnLogOut;
+        private Guna.UI2.WinForms.Guna2Button LogOutBtn;
+        private Guna.UI2.WinForms.Guna2Button guna2Button3;
+        private Guna.UI2.WinForms.Guna2Button guna2Button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel2;
+        private Guna.UI2.WinForms.Guna2Elipse guna2Elipse1;
+        private Guna.UI2.WinForms.Guna2Button closeButton;
+        private Guna.UI2.WinForms.Guna2Button pharm;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
